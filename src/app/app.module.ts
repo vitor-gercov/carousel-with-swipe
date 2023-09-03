817f3b58b476371da75579634dde0eb11fa5a436
Vitor Gobato Gercov <gercovvitor@gmail.com>
2023-09-03 17:41:27 -0300

    implementacao de swipe do carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CarousselComponent } from './components/caroussel/caroussel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarousselComponent
   ],
   imports: [
     BrowserModule
